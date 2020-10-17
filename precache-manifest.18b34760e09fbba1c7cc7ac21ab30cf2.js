95ec54363cca3111978d33c44494243e9f7fd46c
hv10 <noeldanz@gmail.com>
2020-10-17 20:50:05 +0200

    Updates

---
+++ precache-manifest.18b34760e09fbba1c7cc7ac21ab30cf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e677acd351daebbca7bbbfbc8bb202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02eb2f0a240780b715df",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96877942bb7462b0c311",
+    "url": "/static/js/2.db829b3c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.db829b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02eb2f0a240780b715df",
+    "url": "/static/js/main.cdd9ea60.chunk.js"
+  },
+  {
+    "revision": "1f7a015bcfe2e53b1a53",
+    "url": "/static/js/runtime-main.c3f224dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
